cd416e0cf6ea724751c4ff2dd20fc6a98d57d511
uermel <uermel@czii.org>
2024-03-08 09:21:53 -0800

    simple cached zarr reading

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # chimerax-ome-zarr
-OME-Zarr support for ChimeraX
+OME-Zarr support for ChimeraX. Currently only supports **opening** local or remote Zarr files, not saving.
+
+## Installation
+
+1. Install [ChimeraX](https://www.cgl.ucsf.edu/chimerax/download.html)
+2. Download the most recent build from the [releases page.]()
+3. Run the following command in the ChimeraX command prompt to install the plugin:
+```
+toolshed install /path/to/ChimeraX_OME_Zarr-0.1-py3-none-any.whl
+```
+4. Restart ChimeraX
+
+## Usage
+
+This plugin integrates with the ChimeraX `open`-command. Currently, the smallest scale is loaded by default. 
+Users can specify the scale to load using the `scales` flag and a comma-separated list of scale indeces. The remote store
+type can be specified using the `fs` flag. Currently, only `s3` is supported.
+
+Examples:
+
+**Open a local zarr file**
+```
+open /path/to/file.zarr format zarr
+```
+
+**Open a zarr file from S3** (will use default AWS profile, set environment variable `AWS_PROFILE` in the shell that runs
+ChimeraX to change)
+```
+open bucket-name/path/to/file.zarr format zarr fs s3
+```
+
+**Open a zarr file from S3, load 3 scales** 
+```
+open bucket-name/path/to/file.zarr format zarr fs s3 scales 0,1,2
+```
---
+++ src/map_data/zarr_grid.py
@@ -0,0 +1,72 @@
+from chimerax.map_data import GridData
+from chimerax.map_data.datacache import Data_Cache
+import numpy as np
+from typing import Tuple, Any, List
+from zarr.core import Array
+
+
+class ZarrGrid(GridData):
+    # TODO: handle multiscale in here instead of in open.py
+    # TODO: async multiscale fetching: translate the ranges to lower scale in here until data is fetched
+    def __init__(
+        self,
+        array: Array,
+        origin: Tuple[float, float, float] = (0, 0, 0),
+        step: Tuple[float, float, float] = (1, 1, 1),
+        file_type: str = "zarr",
+        path: str = "",
+        name: str = "",
+    ):
+
+        self.dask_data = array
+        dd = array
+
+        shape = dd.shape[::-1]
+        origin = origin[::-1]
+        step = step[::-1]
+
+        GridData.__init__(
+            self,
+            shape,
+            dd.dtype,
+            origin,
+            step,
+            path=path,
+            file_type=file_type,
+            name=name,
+        )
+
+    def read_matrix(
+        self,
+        ijk_origin: Tuple[int, int, int] = (0, 0, 0),
+        ijk_size: Tuple[int, int, int] = None,
+        ijk_step: Tuple[int, int, int] = (1, 1, 1),
+        progress: Any = None,
+    ):
+
+        ijk_origin = ijk_origin[::-1]
+        ijk_step = ijk_step[::-1]
+
+        if ijk_size is None:
+            ijk_size = self.size[::-1]
+        else:
+            ijk_size = ijk_size[::-1]
+            ijk_size = [ijk_origin[i] + ijk_size[i] for i in range(3)]
+
+        print(f"Reading {ijk_origin} {ijk_size} {ijk_step}")
+
+        m = self.dask_data[
+            ijk_origin[0] : ijk_size[0] : ijk_step[0],
+            ijk_origin[1] : ijk_size[1] : ijk_step[1],
+            ijk_origin[2] : ijk_size[2] : ijk_step[2],
+        ]
+
+        print(f"Read {m.shape} {m.dtype}")
+        print(type(m))
+
+        from numpy import float16, float32
+
+        if m.dtype == float16:
+            m = m.astype(float32)
+
+        return m
